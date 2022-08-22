677ffd49830856e046a6bb8800f12ff2d68652c1
Sahyadri Jagtap <109717072+sahyadrij@users.noreply.github.com>
2022-08-22 15:54:42 +0530

    This is my first java program

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+package Sclar;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.print("I am a programmer");
+		System.out.println("Hello World");
+		System.out.println("I love Java");
+	}
+
+}
